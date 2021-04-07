5f813e65999603844c33e43f0edd01989a64af50
Kirill Kopylov <tyroxine@gmail.com>
2021-04-07 21:40:47 +0300

    After first revision

--- HW_1_proxy/run_tests.sh
+++ HW_1_proxy/run_tests.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker-compose up -d
+#docker-compose up -d
 tarantool proxy.lua
 echo "Waiting for tarantool to start"
 sleep 10
@@ -10,7 +10,7 @@ curl -X POST -H "Content-Type: application/json" -d '{"a":"b"}' http://localhost
 curl -X POST -H "Content-Type: application/json" -d '{"a":"b"}' http://localhost:9001/ -o POST_proxied.out
 cat POST_original.out | sed 's/,//g' |  grep -v "host" | grep -v "connection" | sort > POST_original_sorted.out
 cat POST_proxied.out | sed 's/,//g' | grep -v "host" | grep -v "connection" | sort > POST_proxied_sorted.out
-if cmp -s "proxied_sorted.out" "original_sorted.out"; then
+if cmp -s "POST_proxied_sorted.out" "POST_original_sorted.out"; then
     echo "Pass: POST queries identical"
 else
     echo "Fail: POST queries differ"
@@ -21,13 +21,13 @@ curl -X GET -o GET_original.out 'http://localhost:8080/test?a=foo&b=bar'
 curl -X GET -o GET_proxied.out 'http://localhost:9001/test?a=foo&b=bar' 
 cat GET_original.out | sed 's/,//g' |  grep -v "host" | grep -v "connection" | sort > GET_original_sorted.out
 cat GET_proxied.out | sed 's/,//g' | grep -v "host" | grep -v "connection" | sort > GET_proxied_sorted.out
-if cmp -s "proxied_sorted.out" "original_sorted.out"; then
+if cmp -s "GET_proxied_sorted.out" "GET_original_sorted.out"; then
     echo "Pass: GET queries identical"
 else
     echo "Fail: GET queries differ"
 fi
 
 rm -f POST_original.out POST_proxied.out GET_original.out GET_proxied.out GET_original_sorted.out GET_proxied_sorted.out POST_original_sorted.out POST_proxied_sorted.out
-docker-compose down
+#docker-compose down
 
 kill -s SIGTERM $(cat 1.pid)
